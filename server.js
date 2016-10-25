aa1eb4c708e41f43cc997488eb5d750d87fac371
Sergey Trasko <sergey.trasko@gmail.com>
2016-10-25 21:24:11 +0300

    Switched to ES6 + node 6.5.0

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var app = express();
 app.use(express.static(__dirname + '/public'));
 
 
-var server = app.listen(process.env.PORT || 3000, function () {
+var server = app.listen(process.env.PORT || 3000, () => {
     var port = server.address().port;
     console.log('Server started at http://localhost:' + port);
 });
